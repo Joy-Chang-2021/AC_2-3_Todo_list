a2fd1e82af264ca7fcc167603c59644696cf049c
Joy-Chang-2021 <joy50422@gmail.com>
2021-11-11 23:19:15 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect('mongodb://localhost/2-3_todo_list') // 設定與資料庫連線
+const db = mongoose.connection // 儲存連線狀態、透過這個常數使用不同連線狀態的指令
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello world!')
