5c12611c4ae0defe18b328bae6973194567bb902
Joy-Chang-2021 <joy50422@gmail.com>
2021-11-12 00:46:52 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect('mongodb://localhost/2-3_todo_list') // 設定與資料庫連線
 const db = mongoose.connection // 儲存連線狀態、透過這個常數使用不同連線狀態的指令
+
 db.on('error', () => {
   console.log('mongodb error!')
 })
@@ -10,8 +12,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'})) //建立樣板引擎、傳入參數
+app.set('view engine', 'hbs') // 啟用樣板引擎
+
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  res.render('index')
 })
 
 app.listen(3000, () => {
