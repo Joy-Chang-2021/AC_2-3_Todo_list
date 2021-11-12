3b02bce40023e66af7615576a6a50ccbf3fcd9c4
Joy-Chang-2021 <joy50422@gmail.com>
2021-11-12 19:46:24 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect('mongodb://localhost/2-3_todo_list') // 設定與資料庫連線
 const db = mongoose.connection // 儲存連線狀態、透過這個常數使用不同連線狀態的指令
+const Todo = require('./models/todo') // 載入 todo model
 
 db.on('error', () => {
   console.log('mongodb error!')
@@ -16,7 +17,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'})) //建立樣
 app.set('view engine', 'hbs') // 啟用樣板引擎
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取得todo model全部資料，()可傳入參數指定資料
+    .lean() // 將 mongoose 的 model物件轉換成 JS 資料陣列
+    .then(todos => res.render('index', { todos: todos })) // 資料傳入 index 樣板
+    .catch(error => console.error(error)) // 發生錯誤時的處理
 })
 
 app.listen(3000, () => {
