0c8dc64d75e7347afb89bab1689253716cb5edcd
Joy-Chang-2021 <joy50422@gmail.com>
2021-11-12 21:08:48 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect('mongodb://localhost/2-3_todo_list') // 設定與資料庫連線
 const db = mongoose.connection // 儲存連線狀態、透過這個常數使用不同連線狀態的指令
 const Todo = require('./models/todo') // 載入 todo model
+const bodyParser = require('body-parser')
 
 db.on('error', () => {
   console.log('mongodb error!')
@@ -13,14 +14,32 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'})) //建立樣板引擎、傳入參數
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' })) //建立樣板引擎、傳入參數
 app.set('view engine', 'hbs') // 啟用樣板引擎
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理，協助解析 req.body
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   Todo.find() // 取得todo model全部資料，()可傳入參數指定資料
     .lean() // 將 mongoose 的 model物件轉換成 JS 資料陣列
     .then(todos => res.render('index', { todos: todos })) // 資料傳入 index 樣板
     .catch(error => console.error(error)) // 發生錯誤時的處理
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  // 1.
+  // const todo = new Todo({ name: name }) // 建立實體資料
+  // todo.save() // 將實體資料寫入伺服器
+  // 2.
+  return Todo.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
